79123953f9ee3a0b90f2f32d8c357a02f754a839
TMadhan2004 <156100731+TMadhan2004@users.noreply.github.com>
2024-11-01 18:50:55 +0530

    Updated  to display status

--- lib/graph/prim_page.dart
+++ lib/graph/prim_page.dart
@@ -3,117 +3,171 @@ import 'dart:math';
 import 'prim_info_page.dart';
 import 'prim_code_page.dart';
 
-class PrimPage extends StatefulWidget {
+class PrimsPage extends StatefulWidget {
   final List<List<int>> adjacencyList;
 
-  PrimPage({required this.adjacencyList});
+  PrimsPage({required this.adjacencyList});
 
   @override
-  _PrimPageState createState() => _PrimPageState();
+  _PrimsPageState createState() => _PrimsPageState();
 }
 
-class _PrimPageState extends State<PrimPage> with SingleTickerProviderStateMixin {
+class _PrimsPageState extends State<PrimsPage> with SingleTickerProviderStateMixin {
   List<Edge> _edges = [];
   List<Edge> _mst = [];
-  Set<int> _includedVertices = {0}; // Start with vertex 0
-  List<int> _parent = [];
-  int _step = 0;
+  List<Edge> _cycleEdges = [];
+  Set<int> _includedVertices = {}; // Vertices in MST
+  List<Edge> _availableEdges = [];  // Candidate edges for MST expansion
+  int _currentEdgeIndex = 0;
   bool _isRunning = false;
+  bool _hasRun = false;
+  String _statusMessage = '';
   late AnimationController _controller;
   late Animation<double> _animation;
-  String _statusText = 'Start Prim\'s Algorithm';
 
   @override
   void initState() {
     super.initState();
-    _processInput();
-    _edges.sort((a, b) => a.weight.compareTo(b.weight));
-    _parent = List.generate(widget.adjacencyList.length, (index) => index);
+    _initializeEdges();
+    _initializeAnimation();
+  }
 
+  void _initializeEdges() {
+    Set<String> seenEdges = {};
+
+    for (var edge in widget.adjacencyList) {
+      int v1 = edge[0];
+      int v2 = edge[1];
+      int weight = edge[2];
+      String edgeKey = '${min(v1, v2)},${max(v1, v2)}';
+
+      if (!seenEdges.contains(edgeKey)) {
+        _edges.add(Edge(v1, v2, weight));
+        seenEdges.add(edgeKey);
+      }
+    }
+    _edges.sort((a, b) => a.weight.compareTo(b.weight));
+  }
+
+  void _initializeAnimation() {
     _controller = AnimationController(
-      duration: Duration(milliseconds: 1000),
+      duration: Duration(seconds: 4),
       vsync: this,
     );
-
-    _animation = Tween<double>(begin: 0, end: 1).animate(_controller);
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _processNextEdge();
+        }
+      });
   }
 
-  void _processInput() {
-    for (var edge in widget.adjacencyList) {
-      _edges.add(Edge(edge[0], edge[1], edge[2]));
+  void _processNextEdge() {
+    if (_availableEdges.isEmpty && _includedVertices.isNotEmpty) {
+      setState(() {
+        _statusMessage = 'Prim\'s algorithm completed. MST found!';
+        _isRunning = false;
+        _hasRun = true;
+      });
+      return;
+    }
+
+    Edge? minEdge;
+    _availableEdges.sort((a, b) => a.weight.compareTo(b.weight));
+    for (Edge edge in _availableEdges) {
+      // Check if adding the edge would create a cycle
+      if (_includedVertices.contains(edge.vertex1) && _includedVertices.contains(edge.vertex2)) {
+        setState(() {
+          _cycleEdges.add(edge);
+          _statusMessage = 'Cycle detected with edge (${edge.vertex1}, ${edge.vertex2}). Highlighting in red.';
+        });
+      } else if (!_includedVertices.contains(edge.vertex2)) {
+        minEdge = edge;
+        break;
+      }
+    }
+
+    if (minEdge != null) {
+      Edge edge = minEdge;
+      int newVertex = edge.vertex2;
+      setState(() {
+        _mst.add(edge);
+        _includedVertices.add(newVertex);
+        _statusMessage = 'Added edge (${edge.vertex1}, ${edge.vertex2}) to MST.';
+      });
+
+      _availableEdges.addAll(
+        _edges.where((edge) => edge.vertex1 == newVertex && !_includedVertices.contains(edge.vertex2)),
+      );
+      _availableEdges.removeWhere((edge) => edge.vertex2 == newVertex);
+
+      _controller.forward(from: 0);
+    } else {
+      setState(() {
+        _isRunning = false;
+        _hasRun = true;
+        _statusMessage = 'No more edges available. Prim\'s algorithm completed.';
+      });
     }
   }
 
-  Future<void> _startPrim() async {
-    if (_isRunning) return;
+
+  void _startPrims() {
     setState(() {
       _isRunning = true;
-      _statusText = 'Running...';
-    });
-
-    while (_step < _edges.length && _mst.length < widget.adjacencyList.length - 1) {
-      Edge edge = _edges[_step];
-
-      // Check if the edge connects an included vertex to an excluded vertex
-      if ((_includedVertices.contains(edge.vertex1) && !_includedVertices.contains(edge.vertex2)) ||
-          (_includedVertices.contains(edge.vertex2) && !_includedVertices.contains(edge.vertex1))) {
-
-        setState(() {
-          _mst.add(edge);
-          _includedVertices.add(edge.vertex1);
-          _includedVertices.add(edge.vertex2);
-        });
-
-        // Animate each edge addition
-        _controller.reset();
-        await _controller.forward();
-        await Future.delayed(Duration(seconds: 2)); // Delay 2 seconds between each edge
-      }
-
-      _step++;
-    }
-
-    setState(() {
-      _isRunning = false;
-      _statusText = 'Algorithm Completed';
+      _includedVertices.add(0);  // Start from vertex 0 or any arbitrary vertex
+      _availableEdges.addAll(
+          _edges.where((edge) => edge.vertex1 == 0)
+      );
+      _controller.forward();
     });
   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+  void _navigateToInfoPage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => PrimInfoPage()),
+    );
+  }
+
+  void _navigateToCodePage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => PrimsCodePage()),
+    );
+  }
+
+  void _restartPrims() {
+    setState(() {
+      _edges.clear();
+      _mst.clear();
+      _cycleEdges.clear();
+      _includedVertices.clear();
+      _availableEdges.clear();
+      _currentEdgeIndex = 0;
+      _isRunning = false;
+      _hasRun = false;
+      _statusMessage = '';
+      _initializeEdges();
+      _controller.reset();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Prim\'s Visualization'),
+        title: Text('Prim\'s Algorithm Visualization'),
         backgroundColor: Colors.purple,
         foregroundColor: Colors.white,
         actions: [
           IconButton(
             icon: Icon(Icons.info_outline),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => PrimInfoPage(),
-                ),
-              );
-            },
+            onPressed: _navigateToInfoPage,
           ),
           IconButton(
             icon: Icon(Icons.code),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => PrimsCodePage(),
-                ),
-              );
-            },
+            onPressed: _navigateToCodePage,
           ),
         ],
       ),
@@ -124,14 +178,36 @@ class _PrimPageState extends State<PrimPage> with SingleTickerProviderStateMixin
             Expanded(
               flex: 2,
               child: CustomPaint(
-                painter: GraphPainter(_edges, _includedVertices, _mst, _animation.value),
+                painter: GraphPainter(
+                  edges: _edges,
+                  includedVertices: _includedVertices,
+                  mst: _mst,
+                  cycleEdges: _cycleEdges,
+                  animationValue: _animation.value,
+                  currentEdgeIndex: _currentEdgeIndex,
+                ),
                 child: Container(),
               ),
             ),
             SizedBox(height: 20),
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.purple),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Text(
+                _statusMessage,
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(height: 20),
             ElevatedButton(
-              onPressed: _isRunning ? null : _startPrim,
-              child: Text(_statusText),
+              onPressed: (!_isRunning && !_hasRun) ? _startPrims : null,
+              child: Text(!_hasRun
+                  ? 'Start Prim\'s Algorithm'
+                  : 'Algorithm Completed'),
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.purple,
                 foregroundColor: Colors.white,
@@ -140,6 +216,17 @@ class _PrimPageState extends State<PrimPage> with SingleTickerProviderStateMixin
           ],
         ),
       ),
+      floatingActionButton: _hasRun
+          ? Container(
+        alignment: Alignment.bottomLeft,
+        padding: const EdgeInsets.only(left: 30.0, bottom: 0.0), // Adjust bottom padding as needed
+        child: FloatingActionButton(
+          onPressed: _restartPrims,
+          backgroundColor: Colors.purple,
+          child: Icon(Icons.restart_alt, color: Colors.white),
+        ),
+      )
+          : null,
     );
   }
 }
@@ -156,29 +243,34 @@ class GraphPainter extends CustomPainter {
   final List<Edge> edges;
   final Set<int> includedVertices;
   final List<Edge> mst;
+  final List<Edge> cycleEdges;
   final double animationValue;
+  final int currentEdgeIndex;
 
-  GraphPainter(this.edges, this.includedVertices, this.mst, this.animationValue);
+  GraphPainter({
+    required this.edges,
+    required this.includedVertices,
+    required this.mst,
+    required this.cycleEdges,
+    required this.animationValue,
+    required this.currentEdgeIndex,
+  });
 
   @override
+  @override
   void paint(Canvas canvas, Size size) {
-    Paint edgePaint = Paint()
-      ..color = Colors.black
-      ..strokeWidth = 2;
-    Paint mstPaint = Paint()
-      ..color = Colors.green
-      ..strokeWidth = 4;
+    Paint edgePaint = Paint()..color = Colors.black..strokeWidth = 2;
+    Paint mstPaint = Paint()..color = Colors.green.withOpacity(0.6)..strokeWidth = 4;
+    Paint cyclePaint = Paint()..color = Colors.red.withOpacity(0.6)..strokeWidth = 4;
     Paint vertexPaint = Paint()..color = Colors.blue;
-    Paint vertexTextPaint = Paint()
-      ..color = Colors.white
-      ..style = PaintingStyle.fill;
 
-    // Define positions for the vertices dynamically
+
     Map<int, Offset> vertexPositions = {};
-    int vertexCount = edges.fold(0, (currentMax, edge) => max(currentMax, max(edge.vertex1, edge.vertex2))) + 1;
+    int vertexCount = edges.fold(0, (maxV, edge) => max(maxV, max(edge.vertex1, edge.vertex2))) + 1;
     double angleStep = 2 * pi / vertexCount;
     double radius = min(size.width, size.height) / 3;
 
+    // Calculate positions for each vertex
     for (int i = 0; i < vertexCount; i++) {
       double angle = i * angleStep;
       vertexPositions[i] = Offset(
@@ -187,45 +279,47 @@ class GraphPainter extends CustomPainter {
       );
     }
 
-    // Draw edges (original graph)
+    // Draw all edges in black initially
     for (Edge edge in edges) {
       Offset p1 = vertexPositions[edge.vertex1]!;
       Offset p2 = vertexPositions[edge.vertex2]!;
-
-      // Draw the edge
       canvas.drawLine(p1, p2, edgePaint);
 
-      // Label the weight
+      // Draw weight label slightly above each edge
       TextSpan span = TextSpan(style: TextStyle(color: Colors.black), text: '${edge.weight}');
-      TextPainter tp = TextPainter(
-        text: span,
-        textAlign: TextAlign.center,
-        textDirection: TextDirection.ltr,
-      );
+      TextPainter tp = TextPainter(text: span, textAlign: TextAlign.center, textDirection: TextDirection.ltr);
       tp.layout();
+
+      // Offset the midpoint slightly above the line
       Offset midpoint = (p1 + p2) / 2;
-      tp.paint(canvas, midpoint);
+      Offset labelPosition = midpoint + Offset(0, -tp.height / 2 - 15); // Move up by 5 pixels
+
+      tp.paint(canvas, labelPosition);
     }
 
-    // Draw MST edges (in green) with animation
-    for (int i = 0; i < mst.length; i++) {
-      Edge edge = mst[i];
+
+    // Overlay MST edges in green
+    for (Edge edge in mst) {
       Offset p1 = vertexPositions[edge.vertex1]!;
       Offset p2 = vertexPositions[edge.vertex2]!;
-
-      // Gradually draw edges during animation
-      Offset animatedP2 = Offset.lerp(p1, p2, animationValue)!;
-      canvas.drawLine(p1, animatedP2, mstPaint);
+      canvas.drawLine(p1, p2, mstPaint);
     }
 
-    // Draw vertices
+    // Overlay cycle edges in red
+    for (Edge edge in cycleEdges) {
+      Offset p1 = vertexPositions[edge.vertex1]!;
+      Offset p2 = vertexPositions[edge.vertex2]!;
+      canvas.drawLine(p1, p2, cyclePaint);
+    }
+
+    // Draw vertices and labels
     for (var vertex in vertexPositions.keys) {
       Offset position = vertexPositions[vertex]!;
       canvas.drawCircle(position, 20, vertexPaint);
-
-      // Label vertices
-      TextSpan span = TextSpan(style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold), text: '$vertex');
-      TextPainter tp = TextPainter(text: span, textAlign: TextAlign.center, textDirection: TextDirection.ltr);
+      TextSpan span = TextSpan(
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold), text: '$vertex');
+      TextPainter tp = TextPainter(
+          text: span, textAlign: TextAlign.center, textDirection: TextDirection.ltr);
       tp.layout();
       tp.paint(canvas, position - Offset(tp.width / 2, tp.height / 2));
     }
@@ -233,6 +327,6 @@ class GraphPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true; // Repaint on every frame to animate the edges
+    return true;
   }
 }
