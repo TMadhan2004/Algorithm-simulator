39dab9ac376bc4240638291f7fc0f90747f8fc4b
KavinMalarG06 <kavinmalargowthaman@gmail.com>
2024-11-02 17:25:19 +0530

    Update comparison_page.dart

--- lib/sorting/comparison_page.dart
+++ lib/sorting/comparison_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_charts/charts.dart'; // Ensure to import this
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class ComparisonPage extends StatefulWidget {
   final List<String> selectedAlgorithms;
@@ -18,6 +18,9 @@ class ComparisonPage extends StatefulWidget {
 
 class _ComparisonPageState extends State<ComparisonPage> {
   late Map<String, List<int>> _sortResults;
+  String _optimalAlgorithm = '';
+  int _minSwaps = 0;
+  int _maxSwaps = 0;
 
   @override
   void initState() {
@@ -33,254 +36,326 @@ class _ComparisonPageState extends State<ComparisonPage> {
         _sortResults[algorithm] = result;
       });
     }
+    _findOptimalAlgorithm();
+    _findMaxSwaps();
+  }
+
+  void _findOptimalAlgorithm() {
+    int minSwaps = double.maxFinite.toInt();
+    String optimalAlgorithm = '';
+
+    _sortResults.forEach((algorithm, swaps) {
+      int totalSwaps = swaps.reduce((a, b) => a + b);
+      if (totalSwaps < minSwaps) {
+        minSwaps = totalSwaps;
+        optimalAlgorithm = algorithm;
+      }
+    });
+
+    setState(() {
+      _minSwaps = minSwaps;
+      _optimalAlgorithm = optimalAlgorithm;
+    });
+  }
+
+  void _findMaxSwaps() {
+    int maxSwaps = 0;
+
+    _sortResults.forEach((algorithm, swaps) {
+      int totalSwaps = swaps.reduce((a, b) => a + b);
+      if (totalSwaps > maxSwaps) {
+        maxSwaps = totalSwaps;
+      }
+    });
+
+    setState(() {
+      _maxSwaps = maxSwaps;
+    });
   }
 
   Future<List<int>> _sortWithAlgorithm(String algorithm) async {
     List<int> input = List.from(widget.numbers);
-    List<int> steps = List.filled(input.length, 0); // Track the number of swaps
 
     switch (algorithm) {
       case 'Bubble Sort':
-        await _bubbleSort(input, steps);
-        break;
+        return await _bubbleSort(input);
       case 'Selection Sort':
-        await _selectionSort(input, steps);
-        break;
+        return await _selectionSort(input);
       case 'Insertion Sort':
-        await _insertionSort(input, steps);
-        break;
+        return await _insertionSort(input);
       case 'Shell Sort':
-        await _shellSort(input, steps);
-        break;
+        return await _shellSort(input);
       case 'Heap Sort':
-        await _heapSort(input, steps);
-        break;
+        return await _heapSort(input);
       case 'Radix Sort':
-        await _radixSort(input, steps);
-        break;
+        return await _radixSort(input);
       case 'Merge Sort':
-        await _mergeSort(input, steps, 0, input.length - 1);
-        break;
+        return await _mergeSort(input);
       case 'Quick Sort':
-        await _quickSort(input, steps, 0, input.length - 1);
-        break;
+        return await _quickSort(input);
+      default:
+        throw Exception('Unknown sorting algorithm: $algorithm');
     }
-
-    return steps;
   }
 
-  Future<void> _bubbleSort(List<int> arr, List<int> steps) async {
-    for (int i = 0; i < arr.length - 1; i++) {
-      for (int j = 0; j < arr.length - i - 1; j++) {
-        if (arr[j] > arr[j + 1]) {
-          int temp = arr[j];
-          arr[j] = arr[j + 1];
-          arr[j + 1] = temp;
-          steps[j]++;
-          await Future.delayed(Duration(milliseconds: widget.speed.toInt()));
+  Future<List<int>> _bubbleSort(List<int> input) async {
+    List<int> swapCounts = [];
+
+    // Initialize swap counts for each input
+    for (int i = 0; i < input.length; i++) {
+      swapCounts.add(0);
+    }
+
+    for (int i = 0; i < input.length - 1; i++) {
+      int swaps = 0; // Count swaps for this pass
+      for (int j = 0; j < input.length - i - 1; j++) {
+        if (input[j] > input[j + 1]) {
+          // Swap elements
+          int temp = input[j];
+          input[j] = input[j + 1];
+          input[j + 1] = temp;
+          swaps++;
+          swapCounts[j +
+              1]++;
         }
       }
     }
+    return swapCounts;
   }
 
-  Future<void> _selectionSort(List<int> arr, List<int> steps) async {
-    for (int i = 0; i < arr.length - 1; i++) {
-      int minIdx = i;
-      for (int j = i + 1; j < arr.length; j++) {
-        if (arr[j] < arr[minIdx]) {
-          minIdx = j;
+  Future<List<int>> _selectionSort(List<int> input) async {
+    List<int> swapCounts = List.filled(
+        input.length, 0);
+
+    for (int i = 0; i < input.length - 1; i++) {
+      int minIndex = i;
+      for (int j = i + 1; j < input.length; j++) {
+        if (input[j] < input[minIndex]) {
+          minIndex = j;
         }
       }
-      int temp = arr[minIdx];
-      arr[minIdx] = arr[i];
-      arr[i] = temp;
-      steps[i]++;
-      await Future.delayed(Duration(milliseconds: widget.speed.toInt()));
+
+      if (minIndex != i) {
+        int temp = input[i];
+        input[i] = input[minIndex];
+        input[minIndex] = temp;
+
+        swapCounts[i]++;
+        swapCounts[minIndex]++;
+      }
     }
+    return swapCounts;
   }
 
-  Future<void> _insertionSort(List<int> arr, List<int> steps) async {
-    for (int i = 1; i < arr.length; i++) {
-      int key = arr[i];
+  Future<List<int>> _insertionSort(List<int> input) async {
+    List<int> swapCounts = List.filled(
+        input.length, 0);
+    for (int i = 1; i < input.length; i++) {
+      int key = input[i];
       int j = i - 1;
-      while (j >= 0 && arr[j] > key) {
-        arr[j + 1] = arr[j];
+      while (j >= 0 && input[j] > key) {
+        input[j + 1] = input[j];
+        swapCounts[j + 1]++;
         j--;
       }
-      arr[j + 1] = key;
-      steps[i]++;
-      await Future.delayed(Duration(milliseconds: widget.speed.toInt()));
+      input[j + 1] = key;
     }
+    return swapCounts;
   }
 
-  Future<void> _shellSort(List<int> arr, List<int> steps) async {
-    int n = arr.length;
+  Future<List<int>> _mergeSort(List<int> input) async {
+    List<int> swapCounts = List.filled(
+        input.length, 0);
+
+    void merge(List<int> arr, int l, int m, int r) {
+      int n1 = m - l + 1;
+      int n2 = r - m;
+      List<int> L = List.filled(n1, 0);
+      List<int> R = List.filled(n2, 0);
+
+      for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+      for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+      int i = 0,
+          j = 0,
+          k = l;
+      while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+          arr[k] = L[i];
+          i++;
+        } else {
+          arr[k] = R[j];
+          j++;
+          swapCounts[k]++;
+        }
+        k++;
+      }
+      while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+      }
+      while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+      }
+    }
+
+    void sort(List<int> arr, int l, int r) {
+      if (l < r) {
+        int m = l + (r - l) ~/ 2;
+        sort(arr, l, m);
+        sort(arr, m + 1, r);
+        merge(arr, l, m, r);
+      }
+    }
+
+    sort(input, 0, input.length - 1);
+    return swapCounts;
+  }
+
+  Future<List<int>> _shellSort(List<int> input) async {
+    List<int> swapCounts = List.filled(
+        input.length, 0);
+    int n = input.length;
     for (int gap = n ~/ 2; gap > 0; gap ~/= 2) {
       for (int i = gap; i < n; i++) {
-        int temp = arr[i];
+        int temp = input[i];
         int j;
-        for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
-          arr[j] = arr[j - gap];
-          steps[j]++;
-          await Future.delayed(Duration(milliseconds: widget.speed.toInt()));
+        for (j = i; j >= gap && input[j - gap] > temp; j -= gap) {
+          input[j] = input[j - gap];
+          swapCounts[j]++;
         }
-        arr[j] = temp;
+        input[j] = temp;
+        if (j != i) {
+          swapCounts[j]++;
+        }
       }
     }
+    return swapCounts;
   }
 
-  Future<void> _heapSort(List<int> arr, List<int> steps) async {
-    int n = arr.length;
+  Future<List<int>> _heapSort(List<int> input) async {
+    List<int> swapCounts = List.filled(
+        input.length, 0);
+    int n = input.length;
+
+    void heapify(int n, int i) {
+      int largest = i;
+      int left = 2 * i + 1;
+      int right = 2 * i + 2;
+
+      if (left < n && input[left] > input[largest]) {
+        largest = left;
+      }
+      if (right < n && input[right] > input[largest]) {
+        largest = right;
+      }
+      if (largest != i) {
+        // Swap elements
+        int temp = input[i];
+        input[i] = input[largest];
+        input[largest] = temp;
+        swapCounts[i]++;
+        swapCounts[largest]++;
+        heapify(n, largest);
+      }
+    }
 
     for (int i = n ~/ 2 - 1; i >= 0; i--) {
-      await _heapify(arr, n, i, steps);
+      heapify(n, i);
     }
-
-    for (int i = n - 1; i > 0; i--) {
-      int temp = arr[0];
-      arr[0] = arr[i];
-      arr[i] = temp;
-      steps[i]++;
-      await Future.delayed(Duration(milliseconds: widget.speed.toInt()));
-
-      await _heapify(arr, i, 0, steps);
-    }
-  }
-
-  Future<void> _heapify(List<int> arr, int n, int i, List<int> steps) async {
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-
-    if (left < n && arr[left] > arr[largest]) largest = left;
-    if (right < n && arr[right] > arr[largest]) largest = right;
-
-    if (largest != i) {
-      int swap = arr[i];
-      arr[i] = arr[largest];
-      arr[largest] = swap;
-      steps[i]++;
-      await Future.delayed(Duration(milliseconds: widget.speed.toInt()));
-      await _heapify(arr, n, largest, steps);
-    }
-  }
-
-  Future<void> _radixSort(List<int> arr, List<int> steps) async {
-    int max = arr.reduce((a, b) => a > b ? a : b);
-    for (int exp = 1; max ~/ exp > 0; exp *= 10) {
-      await _countSort(arr, exp, steps);
-    }
-  }
-
-  Future<void> _countSort(List<int> arr, int exp, List<int> steps) async {
-    int n = arr.length;
-    List<int> output = List.filled(n, 0);
-    List<int> count = List.filled(10, 0);
-
-    for (int i = 0; i < n; i++) {
-      count[(arr[i] ~/ exp) % 10]++;
-    }
-
-    for (int i = 1; i < 10; i++) {
-      count[i] += count[i - 1];
-    }
-
     for (int i = n - 1; i >= 0; i--) {
-      output[count[(arr[i] ~/ exp) % 10] - 1] = arr[i];
-      steps[i]++;
-      await Future.delayed(Duration(milliseconds: widget.speed.toInt()));
-      count[(arr[i] ~/ exp) % 10]--;
+      int temp = input[0];
+      input[0] = input[i];
+      input[i] = temp;
+      swapCounts[0]++;
+      swapCounts[i]++;
+      heapify(i, 0);
     }
-
-    for (int i = 0; i < n; i++) {
-      arr[i] = output[i];
-    }
+    return swapCounts;
   }
 
-  Future<void> _mergeSort(List<int> arr, List<int> steps, int left, int right) async {
-    if (left < right) {
-      int middle = (left + right) ~/ 2;
+  Future<List<int>> _quickSort(List<int> input) async {
+    List<int> swapCounts = List.filled(
+        input.length, 0);
 
-      await _mergeSort(arr, steps, left, middle);
-      await _mergeSort(arr, steps, middle + 1, right);
-
-      await _merge(arr, steps, left, middle, right);
-    }
-  }
-
-  Future<void> _merge(List<int> arr, List<int> steps, int left, int middle, int right) async {
-    int n1 = middle - left + 1;
-    int n2 = right - middle;
-
-    List<int> leftArray = List.filled(n1, 0);
-    List<int> rightArray = List.filled(n2, 0);
-
-    for (int i = 0; i < n1; i++) {
-      leftArray[i] = arr[left + i];
-    }
-    for (int j = 0; j < n2; j++) {
-      rightArray[j] = arr[middle + 1 + j];
-    }
-
-    int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-      if (leftArray[i] <= rightArray[j]) {
-        arr[k] = leftArray[i];
-        i++;
-      } else {
-        arr[k] = rightArray[j];
-        j++;
+    int _partition(List<int> arr, int low, int high) {
+      int pivot = arr[high];
+      int i = (low - 1);
+      for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+          i++;
+          int temp = arr[i];
+          arr[i] = arr[j];
+          arr[j] = temp;
+          swapCounts[i]++;
+          swapCounts[j]++;
+        }
       }
-      steps[k]++;
-      await Future.delayed(Duration(milliseconds: widget.speed.toInt()));
-      k++;
+      int temp = arr[i + 1];
+      arr[i + 1] = arr[high];
+      arr[high] = temp;
+      swapCounts[i + 1]++;
+      swapCounts[high]++;
+      return i + 1;
     }
 
-    while (i < n1) {
-      arr[k] = leftArray[i];
-      i++;
-      k++;
+    void quickSort(List<int> arr, int low, int high) {
+      if (low < high) {
+        int pi = _partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+      }
     }
-
-    while (j < n2) {
-      arr[k] = rightArray[j];
-      j++;
-      k++;
-    }
-  }
-  Future<void> _quickSort(List<int> arr, List<int> steps, int low, int high) async {
-    if (low < high) {
-      int pi = await _partition(arr, low, high, steps);
-      await Future.wait([
-        _quickSort(arr, steps, low, pi - 1),
-        _quickSort(arr, steps, pi + 1, high),
-      ]);
-    }
+    quickSort(input, 0, input.length - 1);
+    return swapCounts;
   }
 
-  Future<int> _partition(List<int> arr, int low, int high, List<int> steps) async {
-    int pivot = arr[high];
-    int i = low - 1;
+  Future<List<int>> _radixSort(List<int> input) async {
+    List<int> swapCounts = [];
+    if (input.isEmpty) return swapCounts;
 
-    for (int j = low; j < high; j++) {
-      if (arr[j] <= pivot) {
-        i++;
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-        steps[j]++;
-        await Future.delayed(Duration(milliseconds: widget.speed.toInt()));
+    int max = input.reduce((a, b) => a > b ? a : b);
+
+    for (int exp = 1; max ~/ exp > 0; exp *= 10) {
+      List<int> output = List.filled(input.length, 0);
+      int countSize = 10;
+      List<int> count = List.filled(countSize, 0);
+
+      for (int i = 0; i < input.length; i++) {
+        int digit = (input[i] ~/ exp) % 10;
+        count[digit]++;
+      }
+
+      for (int i = 1; i < countSize; i++) {
+        count[i] += count[i - 1];
+      }
+
+      int operations = 0;
+
+      for (int i = input.length - 1; i >= 0; i--) {
+        int digit = (input[i] ~/ exp) % 10;
+        output[count[digit] - 1] = input[i];
+        count[digit]--;
+        operations++;
+      }
+
+      swapCounts.add(operations);
+
+      for (int i = 0; i < input.length; i++) {
+        input[i] = output[i];
       }
     }
 
-    int temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
-    steps[i + 1]++;
-    await Future.delayed(Duration(milliseconds: widget.speed.toInt()));
+    return swapCounts;
+  }
 
-    return i + 1;
-  }                                                                                                                                                                           @override
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -289,22 +364,50 @@ class _ComparisonPageState extends State<ComparisonPage> {
       body: Center(
         child: _sortResults.isEmpty
             ? CircularProgressIndicator()
-            : SfCartesianChart(
-          title: ChartTitle(text: 'Comparison of Sorting Algorithms'),
-          legend: Legend(isVisible: true),
-          primaryXAxis: CategoryAxis(),
-          primaryYAxis: NumericAxis(title: AxisTitle(text: 'Number of Swaps')),
-          series: _sortResults.entries.map((entry) {
-            return LineSeries<_ChartData, int>(
-              dataSource: List.generate(
-                entry.value.length,
-                    (index) => _ChartData(widget.numbers[index], entry.value[index]),
+            : Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Optimal Algorithm: $_optimalAlgorithm\nMinimum Swaps: $_minSwaps',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
               ),
-              xValueMapper: (_ChartData data, _) => data.x,
-              yValueMapper: (_ChartData data, _) => data.y,
-              name: entry.key,
-            );
-          }).toList(),
+            ),
+            Expanded(
+              child: SfCartesianChart(
+                title: ChartTitle(text: 'Comparison of Sorting Algorithms'),
+                legend: Legend(isVisible: true),
+                primaryXAxis: CategoryAxis(
+                  title: AxisTitle(text: 'Input Values'),
+                  interval: 1,
+                  maximumLabels: widget.numbers.length,
+                ),
+                primaryYAxis: NumericAxis(
+                  title: AxisTitle(text: 'Number of Swaps'),
+                  maximum: widget.numbers.length.toDouble(),
+                  interval: (widget.numbers.length > 0 ? (widget.numbers.length / 5).ceilToDouble() : 1),
+                ),
+                series: _sortResults.entries.map((entry) {
+                  return LineSeries<_ChartData, String>(
+                    dataSource: List.generate(
+                      widget.numbers.length,
+                          (index) {
+                        int operationsCount = index < entry.value.length ? entry.value[index] : 0;
+                        return _ChartData(widget.numbers[index].toString(), operationsCount);
+                      },
+                    ),
+                    xValueMapper: (_ChartData data, _) => data.x,
+                    yValueMapper: (_ChartData data, _) => data.y,
+                    name: entry.key,
+                  );
+                }).toList(),
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -312,8 +415,7 @@ class _ComparisonPageState extends State<ComparisonPage> {
 }
 
 class _ChartData {
-  final int x;
-  final int y;
-
   _ChartData(this.x, this.y);
+  final String x;
+  final int y; 
 }
